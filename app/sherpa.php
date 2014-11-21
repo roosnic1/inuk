2d105bc4065e3d90a1ec80c890156079070abfb0
Claudio Vögtli <cv@layzapp.com>
2014-11-21 13:22:25 +0100

    two new product pages

---
+++ app/sherpa.php
@@ -0,0 +1,136 @@
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/_/inc/init.php'); ?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js ie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie ie9 lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <title></title>
+    <meta name="description" content="">
+    <?php require("_/inc/head.php"); ?>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <?php require('_/inc/header.php'); ?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-4">
+                        <img src="/_/img/stamp.png" class="koProdImg" />
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12 koDesc">
+                        <h1 class="text-center">Sherpa</h1>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="koProdBreak"></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <img src="/_/img/sherpa/img1.jpg" class="koProdImg" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="koProdBreak"></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-5 col-xs-12 koDesc2">
+                        <p>Sherpa ist ein kleinen Helfer zum Tragen von schweren Packeten oder Altpapierbündeln. Das Bambusrohr verhindert das Einschneiden der Schnur in die Hand und verteilt den Druck auf eine grössere Fläche.</p>
+
+                        <table>
+                          <tr>
+                            <td><p>Grösse:</p></td>
+                            <td><p>ca. 12cm lang, 3.5cm Durchmesser</p></td>
+                          </tr>
+                          <tr>
+                            <td><p>Material:</p></td>
+                            <td><p>Bambus</p></td>
+                          </tr>
+                          <tr>
+                            <td><p>Jahr:</p></td>
+                            <td><p>2014</p></td>
+                          </tr>
+                          <tr>
+                            <td><p>Design:</p></td>
+                            <td><p>Jacqueline Amacher</p></td>
+                          </tr>
+                        </table>
+                    </div>
+                    <div class="col-xs-12 visible-xs-block">
+                        <div class="koProdBreak"></div>
+
+                    </div>
+                    <div class="col-sm-7 col-xs-12">
+                        <img src="/_/img/sherpa/img2.jpg" class="koProdImg" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="koProdBreak"></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <img src="/_/img/sherpa/img3.jpg" class="koProdImg" />
+                    </div>
+                    <div class="col-xs-12 visible-xs-block">
+                        <div class="koProdBreak"></div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <img src="/_/img/sherpa/img4.jpg" class="koProdImg" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="koProdBreak"></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <img src="/_/img/sherpa/img5.jpg" class="koProdImg" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <!--[if lt IE 9]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser.
+        Please <a href="http://browsehappy.com/">upgrade your browser</a>
+        or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <?php require('_/inc/footer.php'); ?>
+
+    <!-- JAVASCRIPT -->
+    <?php require('_/inc/analytics.php'); ?>
+
+    <?php require('_/inc/tail.php'); ?>
+</body>
+</html>
