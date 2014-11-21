b1dda71bbba0d0a5ebdab8810394c929e2d71c11
Nicolas Roos <nicolas.k.roos@gmail.com>
2014-11-21 13:45:40 +0100

    new products

--- app/index.php
+++ app/index.php
@@ -23,8 +23,8 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-5 koMouseOver">
                         <a href="/vittoria.php">
-                            <img src="/_/img/preview_vittoria.png" class="koProdImg" />
-                            <div class="koProdInfo">
+                            <img src="/_/img/preview_vittoria.jpg" class="koProdImg" />
+                            <div class="koProdInfo koPIVittoria">
                                 <h1 class="text-center">Vittoria</h1>
                                 <div>
                                     <h3 class="text-center">espressotasse</h3>
@@ -38,7 +38,7 @@
                     </div>
                     <div class="col-xs-12 col-sm-7 koMouseOver">
                         <a href="/zeugen.php">
-                            <img src="/_/img/preview_zeugen.png" class="koProdImg" />
+                            <img src="/_/img/preview_zeugen.jpg" class="koProdImg" />
                             <div class="koProdInfo">
                                 <h1 class="text-center">Zeugen</h1>
                                 <div>
@@ -59,7 +59,7 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-7 koMouseOver">
                         <a href="/avalu.php">
-                            <img src="/_/img/preview_avalu.png" class="koProdImg" />
+                            <img src="/_/img/preview_avalu.jpg" class="koProdImg" />
                             <div class="koProdInfo">
                                 <h1 class="text-center">Avalu</h1>
                                 <div>
@@ -74,11 +74,47 @@
                     </div>
                     <div class="col-xs-12 col-sm-5 koMouseOver">
                         <a href="/waldtiere.php">
-                            <img src="/_/img/preview_waldtiere.png" class="koProdImg" />
+                            <img src="/_/img/preview_waldtiere.jpg" class="koProdImg" />
                             <div class="koProdInfo">
                                 <h1 class="text-center">Waldtiere</h1>
                                 <div>
                                     <h3 class="text-center">Karten</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="koProdBreak"></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-5 koMouseOver">
+                        <a href="/sherpa.php">
+                            <img src="/_/img/preview_sherpa.jpg" class="koProdImg" />
+                            <div class="koProdInfo">
+                                <h1 class="text-center">Sherpa</h1>
+                                <div>
+                                    <h3 class="text-center">Altpapierträger</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-xs-12 visible-xs-block">
+                        <div class="koProdBreak"></div>
+
+                    </div>
+                    <div class="col-xs-12 col-sm-7 koMouseOver">
+                        <a href="/sir.php">
+                            <img src="/_/img/preview_sir.jpg" class="koProdImg" />
+                            <div class="koProdInfo">
+                                <h1 class="text-center">Sir</h1>
+                                <div>
+                                    <h3 class="text-center">Münzbeutel</h3>
                                 </div>
                             </div>
                         </a>
