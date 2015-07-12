2a2e885af3c9f7abbe5457cf4b408b7dc74abcf1
Claudio Vögtli <cv@layzapp.com>
2015-07-12 17:25:43 +0200

    pdf's and new section PRESS

---
+++ app/presse.php
@@ -0,0 +1,80 @@
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/_/inc/init.php'); ?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js ie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie ie9 lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <title></title>
+    <meta name="description" content="Produkt Design Luzern, Jacqueline Amacher, Humberto Rapelli">
+    <?php require("_/inc/head.php"); ?>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <?php require('_/inc/header.php'); ?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-4">
+                        <img src="/_/img/stamp_inuk.png" class="koProdImg" />
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12 koDesc">
+                        <h1 class="text-center">inuk in der Presse</h1>
+                        <p class="text-center">Ein Ausschnitt aus verschiedene Presseartikel in welchen inuk erschienen ist.</p>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="koProdBreak"></div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1 koKollektiv">
+                        <h4 class="text-center">Humberto Rapelli & Jacqueline Amacher</h4>
+                       <p class="text-center"><a href="http://inuk-kollektiv.ch/_/pdf/Gutes_Design_ist_verstaendlich.pdf
+">Neue Luzerner Zeitung – Gutes Design ist verständlich</a></p>
+                        <p class="text-center"><a href="http://inuk-kollektiv.ch/_/pdf/luzerner_rundschau.pdf
+">Luzerner Rundschau – Zwischen Sinnlichkeit und Sinngebung</a></p>
+                       <p class="text-center"><a href="https://www.homify.de/ideenbuecher/19761/regale-im-bauhausstil
+">Homify – Regale im Bauhausstil</a></p>
+                        <p class="text-center"><a href="https://www.homify.de/ideenbuecher/19515/individuelle-moebel
+">Homify – Individuelle Möbel</a></p>
+                        <p class="text-center"><a href="https://www.homify.de/ideenbuecher/26623/geschenkideen-zum-vatertag
+">Homify – Geschenkideen zum Vatertag</a></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <!--[if lt IE 9]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser.
+        Please <a href="http://browsehappy.com/">upgrade your browser</a>
+        or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <?php require('_/inc/footer.php'); ?>
+
+    <!-- JAVASCRIPT -->
+    <?php require('_/inc/analytics.php'); ?>
+
+    <?php require('_/inc/tail.php'); ?>
+</body>
+</html>
